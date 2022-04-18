32636d4f3cdb317cebe52a6ea80fd04c7c8a4fc2
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-04-18 17:21:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var type = new Typed(".typing-text", {
+  strings: [
+    "Web Designer",
+    "Front end developer",
+    "backend developer",
+    "full stack developer",
+  ],
+  typeSpeed: 120,
+  loop: true,
+});
